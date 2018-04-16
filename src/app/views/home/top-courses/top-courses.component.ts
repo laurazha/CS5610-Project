1c72fced8623e5d4143e2abd0ce6d75387d8bfa3
laurazha <zhaxs820@gmail.com>
2018-04-15 17:47:42 -0700

    top courses

--- src/app/views/home/top-courses/top-courses.component.ts
+++ src/app/views/home/top-courses/top-courses.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopCoursesComponent implements OnInit {
 
+  courses = [{name: 'CS5001', title: 'Racket', rating: 9.3, numRating: 3},
+    {name: 'CS5002', title: 'Data Structure', rating: 9.0, numRating: 6},
+    {name: 'CS5003', title: 'OOD', rating: 8.2, numRating: 2}];
+
   constructor() { }
 
   ngOnInit() {
