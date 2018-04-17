b5262769187d263c6f3bc91712abd180231e7501
Felix <wufeiyue@WUFEIYUEdeMacBook-Air.local>
2018-04-17 12:49:22 -0700

    update

---
+++ server/models/course/course.schema.server.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var CourseSchema = mongoose.Schema({
+  _user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  name: String,
+  description: String,
+  title: String,
+  rating: String,
+  widgets: [{
+   type: mongoose.Schema.Type.ObjectId,
+   ref: 'Widget'
+  }],
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  }
+}, {collection: 'course'});
+
+module.exports = CourseSchema;
