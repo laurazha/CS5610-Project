4ed0b6254c789cc8a28d4954ddf62a6fcb2a6f4a
jielu <radiancelu@gmail.com>
2018-04-17 11:18:51 -0700

    add server side for students and professors dashboard

--- src/app/views/user/student/student-dashboard/student-dashboard.component.ts
+++ src/app/views/user/student/student-dashboard/student-dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../../models/user.model.client';
+import {UserService} from '../../../../services/user.service.client';
+import {SharedService} from '../../../../services/shared.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-student-dashboard',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-dashboard.component.css']
 })
 export class StudentDashboardComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private sharedService: SharedService,
+              public router: Router) { }
+
+
+  logout() {
+    this.userService.logout()
+      .subscribe(
+        (data: any) => this.router.navigate(['/login'])
+      );
+  }
 
   ngOnInit() {
+    this.user = this.sharedService.user;
   }
 
 }
