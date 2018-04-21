0108ca161cd10b234cdf9c5d8cdfa5a68599c52d
laurazha <zhaxs820@gmail.com>
2018-04-20 20:09:47 -0700

    deployable

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 // var db = mongoose.connect('mongodb://localhost:27017/cs5610project');
-var db = mongoose.connect('mongodb://laurazha_cs5610hw:cs5610@ds251889.mlab.com:51889/heroku_2j2bj958');
+var db = mongoose.connect('mongodb://laurazha_cs5610project:cs5610@ds251889.mlab.com:51889/heroku_2j2bj958');
 
 module.exports = db;
